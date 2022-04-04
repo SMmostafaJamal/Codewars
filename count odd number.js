7af1f0ab9b44dd72b8b4c0ea5e0a9f3c20323eac
SMmostafaJamal <sm.mostafajamal@gmail.com>
2022-04-05 01:04:22 +0600

    8kyu algo

--- count odd number.js
+++ count odd number.js
@@ -1,17 +1,23 @@
 //Given a number n, return the number of positive odd numbers below n, EASY!
 
 function oddCount(n){
-  // your code here
-  let oddNumber = [];
-
-  for(let i = 1; i < n; i++){  
-    if(i % 2 !== 0){
-      oddNumber.push(i)
-    }
-  } 
-  return oddNumber.length
-  
+  return Math.round((n-1)/2)
 }
 
-oddCount(15)
+oddCount(1527703033000150)
+
+// function oddCount(n){
+//   // your code here
+//   let oddNumber = [];
+
+//   for(let i = 1; i < n; i++){  
+//     if(i % 2 !== 0){
+//       oddNumber.push(i)
+//     }
+//   } 
+//   return oddNumber.length
+  
+// }
+
+// oddCount(15)
 
