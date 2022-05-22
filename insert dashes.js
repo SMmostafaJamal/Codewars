3869b6963d43cdaee3d0235166511ef841ad719e
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-05-22 17:43:28 +0600

    7kyu

--- insert dashes.js
+++ insert dashes.js
@@ -2,4 +2,23 @@
 
 Note that the number will always be non-negative (>= 0).
 */
+function insertDash(num) {
+   //code me
+  let strNum = String(num)
+  let str = `${strNum[0]}`
+  for(let i =0; i < strNum.length-1; i++){
+    for(let j = i+1; j<strNum.length; j++){
+//     console.log(strNum[j], strNum[strNum.length-1])
+      if(strNum[i] % 2 != 0 && strNum[j]%2 != 0){
+     str +=  '-' +strNum[j] 
+    }else{
+      str += strNum[j] 
+    }
+      ++i
 
+    }
+  }
+  return str
+}
+insertDash(454793)
+// insertDash(1003567)
