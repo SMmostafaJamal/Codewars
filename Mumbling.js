54f62a8d7f148cdc652047d34aa2b102fda7f31e
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-06-02 14:45:26 +0600

    7kyu

---
+++ Mumbling.js
@@ -0,0 +1,16 @@
+//  writing a function for accum
+
+// P: string, no integar
+// R: string with first letter is cap and will use '-' in each letter and increase one letter on each letter
+// E: accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// P:
+
+// a function which receives string
+function accum(s) {
+//   split each letter 
+  s = s.toLowerCase().split('')
+//   loop through each letter and use uppercase & increase one letter in each loop join by'-'
+  return s.map((el,i)=> el.toUpperCase()+(el.repeat(i))).join('-')
+}
+
+accum("RqaEzty") // -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
