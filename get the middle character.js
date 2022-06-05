bb9cae01cd586fd49521dba3f04fbc56f9cacdad
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-06-05 16:37:04 +0600

    7kyu

--- get the middle character.js
+++ get the middle character.js
@@ -1,2 +1,21 @@
-// 
-You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters
+// given a word should return the middle character of the word if it's odd return middle character if even return 2 middle character
+
+// P: string 
+// R: string, middle character
+// E: Kata.getMiddle("testing") should return "t" Kata.getMiddle("middle") should return "dd"
+// P:
+
+// create a function which receives string
+function getMiddle(s){
+//   if its even then return two middle character or else return middle character 
+  return s.length % 2 ===0 ?  s[(s.length/2)-1] + s[s.length/2]  
+                    : s[Math.floor(s.length/2)]
+     
+  
+}
+
+getMiddle("middle") // 'dd'
+getMiddle("test") // es
+
+
+getMiddle("testing")
