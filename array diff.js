b4d2a4b89def49f4cb18d706367feb491b66a0e9
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-07-09 03:49:33 +0600

    6kyu

--- array diff.js
+++ array diff.js
@@ -9,3 +9,32 @@ If a value is present in b, all of its occurrences must be removed from the othe
 arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 */
+function arrayDiff(a, b) {
+  
+  for(let i=0; i < b.length; i++){
+    for(let j =0; j < a.length;j++){
+      
+     if(a.includes(b[i])) a.splice(a.indexOf(b[i]), 1) 
+      
+    }
+  }
+
+   return a
+}
+arrayDiff([1,2,2], [2])
+
+/*
+function arrayDiff(a, b) {
+  
+  for(let i=0; i < b.length; i++){
+    for(let j =0; j < a.length;j++){
+     if((b[i]) === (a[j])) a.splice(a.indexOf(a[j]), 1) 
+      }
+    }
+  
+   b.forEach(el => { if(a.includes(el)) a.splice(a.indexOf(el),1) })
+
+   return a
+}
+arrayDiff([1,2,2], [2])
+*/
